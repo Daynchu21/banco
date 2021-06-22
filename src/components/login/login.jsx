458cd15da0da4ffdc01d35ba28b910f1f4c758d9
Alexis Arias <Alexissa41@gmail.com>
2021-06-22 19:37:22 -0300

    IsBigScreen Function not used

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -25,7 +25,7 @@ export default function Login() {
         query: '(max-device-width: 1224px)'
     })
     
-const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' })
+// const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' })
    const [Enviado,SetEnviado] = useState(false)
 //    const { isLoggedIn } = useSelector((state) => state.auth);
 //    const { message } = useSelector((state) => state.message);
