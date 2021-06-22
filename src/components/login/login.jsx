926d8e2154f78f26d8957751ff5d686fcbe540cc
David <C06425@bh.com.ar>
2021-06-22 11:08:35 -0300

    cambios banco

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,17 +1,13 @@
-//http://backend-unificado-canales-desa.appd.bh.com.ar/ob/api/login
-
-//     http://localhost:8080/ob/api/login
-//     JSON 
-//     {​​​​​​​
-//     "usuario": "Febrero04",
-//     "clave": "40000004"
-// }​​​​​​​
-
-
-
+import './login.scss'
+import './../modulos/button/button.scss'
+import Button from './../modulos/button/button'
 import React,{useState} from 'react'
-const axios = require('axios');
-const Swal = require('sweetalert2')
+import { useDispatch } from "react-redux";
+import { login } from "../../actions/auth";
+import { useMediaQuery } from 'react-responsive'
+
+// const axios = require('axios');
+// const Swal = require('sweetalert2')
 
 
 export default function Login() {
@@ -20,7 +16,17 @@ export default function Login() {
         Usuario: '',
         Pass: ''
     })
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-device-width: 1224px)'
+      })
+      const isTabletOrMobileDevice = useMediaQuery({
+        query: '(max-device-width: 1224px)'
+      })
    const [Enviado,SetEnviado] = useState(false)
+//    const { isLoggedIn } = useSelector((state) => state.auth);
+//    const { message } = useSelector((state) => state.message);
+   const dispatch = useDispatch();
+
 
    const handleChange =(e) =>{
     SetDatosUsuarios({
@@ -33,60 +39,110 @@ export default function Login() {
     e.preventDefault()
     SetEnviado(true)
     console.log(process.env )
+
     if (!DatosUsuario.Pass && !DatosUsuario.Usuario)
     {
         console.log("por favor complete los datos")
     }else
     {
-       axios.post('http://backend-unificado-canales-desa.appd.bh.com.ar/ob/api/login', {
-        usuario: DatosUsuario.Usuario,
-        clave: DatosUsuario.Pass
+        dispatch(login(DatosUsuario.Usuario,DatosUsuario.Pass))
+        .then(() => {
+            // props.history.push("/");
+            window.location.reload();
           })
-          .then(function (response) {
-              if((response.data).hasOwnProperty('datos'))
-              {
-                Swal.fire('Login Exitoso', response.data.datos.cuit, 'success')
-              }else
-              {
-                Swal.fire('', "Los datos ingresados no son válidos", 'error')
-              }
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
+          .catch(() => {
+           
           });
+
+
+    //    axios.post('http://backend-unificado-canales-desa.appd.bh.com.ar/ob/api/login', {
+    //     usuario: DatosUsuario.Usuario,
+    //     clave: DatosUsuario.Pass
+    //       })
+    //       .then(function (response) {
+    //           if((response.data).hasOwnProperty('datos'))
+    //           {
+    //             Swal.fire('Login Exitoso', response.data.datos.cuit, 'success')
+    //           }else
+    //           {
+    //             Swal.fire('', "Los datos ingresados no son válidos", 'error')
+    //           }
+    //         console.log(response);
+    //       })
+    //       .catch(function (error) {
+    //         console.log(error);
+    //       });
     }
 
 }
 
-
-
 return(
-    <div style={{ background: "#fbfbfb",borderradius: "8px",boxshadow: "1px 2px 8px rgba(0, 0, 0, 0.65)",height: "410px",margin: "6rem auto 8.1rem auto",width: "329px"}}>
-        <div style={{padding: "12px 55px"}}>
-         <div className="col-md-6 col-md-offset-3">
-                <h2 style={{paddingLeft: "20%"}}>Login</h2>
-                <form name="form" onSubmit={handleSubmit} style={{ display: "flex",flexDirection: "column"}}>
-                    <div className={'form-group' + ( !DatosUsuario.Usuario ? ' has-error' : '')} >
-                        <label htmlFor="username" style={{paddingtop:"13px"}}>Usuario</label>
-                        <input type="text" className="form-control" name="Usuario"  onChange={handleChange} />
-                        {Enviado && !DatosUsuario.Usuario &&
-                            <div className="help-block" style={{color:"red"}}>Ingrese Usuario</div>
-                        }
-                    </div>
-                    <div className={'form-group' + ( !DatosUsuario.Pass ? ' has-error' : '')} style={{"padding-top": "20px", "padding-bottom": "20px"}}>
-                        <label htmlFor="password">Contraseña</label>
-                        <input type="password" className="form-control" name="Pass"  onChange={handleChange} />
-                        { Enviado && !DatosUsuario.Pass &&
-                            <div className="help-block" style={{color:"red"}}>Ingrese Contraseña</div>
-                        }
-                    </div>
-                    <div className="form-group" style={{"padding-left": "50px"}}>
-                        <button className="btn btn-primary" >Login</button>
-                    </div>
-                </form>
-            </div>
+    <div>
+    {isDesktopOrLaptop && <>
+    <div style={{width: "1440px",height: "1024px",color:"black"}}>
+        <div style={{color: "white",width: "432px",height: "606px",left: "504px",top: "212px",background: "white"}}>
+            <form name="form" onSubmit={handleSubmit} >
+            <h1 style={{margin:"16px"}}>
+                ¡Bienvenido a Office Banking!
+                </h1>
+                <div className={'form-group' + ( !DatosUsuario.Usuario ? ' has-error' : '')} >
+                    <input type="text" className="form-control" name="Usuario" placeholder="Usuario"  onChange={handleChange} />
+                    {Enviado && !DatosUsuario.Usuario &&
+                        <div className="help-block" style={{color:"red"}}>Ingrese Usuario</div>
+                    }
+                </div>
+                <div className={'form-group' + ( !DatosUsuario.Pass ? ' has-error' : '')} >
+                    <input type="password" className="form-control" name="Pass" placeholder="Contraseña" onChange={handleChange} />
+                    { Enviado && !DatosUsuario.Pass &&
+                        <div className="help-block" style={{color:"red"}}>Ingrese Contraseña</div>
+                    }
+                </div>
+                <input type="checkbox" name="my-checkbox" id="opt-in" />
+                <label for="opt-in">Ingrese Contraseña</label>
+                
+                <div className="form-group" >
+                    {/* <button className="btn btn-primary" >Login</button> */}
+                    <Button name="botonVerde" text="Iniciar sesión"/> 
+                    <Button name="botonBlanco" text="Crear cuenta empresa" />                     
+                </div>
+            </form>
         </div>
+    </div>
+    </>}
+    {isTabletOrMobileDevice &&
+    
+    <form name="form" onSubmit={handleSubmit} >
+            <h1 style={{margin:"16px"}}>
+                ¡Bienvenido a Office Banking!
+                </h1>
+                <div className={'form-group' + ( !DatosUsuario.Usuario ? ' has-error' : '')} >
+                    <input type="text" className="form-control" name="Usuario" placeholder="Usuario"  onChange={handleChange} />
+                    {Enviado && !DatosUsuario.Usuario &&
+                        <div className="help-block" style={{color:"red"}}>Ingrese Usuario</div>
+                    }
+                </div>
+                <div className={'form-group' + ( !DatosUsuario.Pass ? ' has-error' : '')} >
+                    <input type="password" className="form-control" name="Pass" placeholder="Contraseña" onChange={handleChange} />
+                    { Enviado && !DatosUsuario.Pass &&
+                        <div className="help-block" style={{color:"red"}}>Ingrese Contraseña</div>
+                    }
+                </div>
+                <div className="container_check">
+                    <div className="left_check">
+                <input type="checkbox" name="my-checkbox" id="opt-in" />
+                </div>
+                <div className="right_check">
+                <label>Recordar mi usuario</label>
+                </div>
+                </div>
+                <div className="form-group" >
+                    {/* <button className="btn btn-primary" >Login</button> */}
+                    <Button name="botonVerde" text="Iniciar sesión"/> 
+                    <Button name="botonBlanco" text="Crear cuenta empresa" />                     
+                </div>
+            </form>
+
+    }
     </div>
 ) 
     
