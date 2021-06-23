975d93fed96c9c8b61fa923418f485b62e4b6751
Alexis Arias <Alexissa41@gmail.com>
2021-06-23 16:01:52 -0300

    Cambios de Router a hashRouter, redirección a Login correcta. En rutas privadas utilizar Router para rutas client-side no server-side.

--- src/routes/RouterPublic.js
+++ src/routes/RouterPublic.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import Login from "../components/login/login.js";
-import { Router, Redirect, Switch } from "react-router-dom";
-import { history } from "../helpers/history";
+import { HashRouter, Redirect, Switch } from "react-router-dom";
 import { EmptyLayout, LayoutRoute } from "../layout/index.js";
 
 export const RouterPublic = () => {
   return (
-    <Router history={history}>
+    /**Se hizo el cambio a hashROuter para manejar rutas fisicas en el redirect. En rutas privadas
+     * utilizar rutas normales.
+     */
+    <HashRouter>
       <Switch>
         {/*Se cambia a layoute route para incluir estilos globales como temas en las rutas */}
         <LayoutRoute
@@ -16,8 +18,9 @@ export const RouterPublic = () => {
           layout={EmptyLayout}
           component={Login}
         />
+
         <Redirect to="/login" />
       </Switch>
-    </Router>
+    </HashRouter>
   );
 };
